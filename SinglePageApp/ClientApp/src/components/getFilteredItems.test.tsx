09100bb52cb292f667bdeb20c41cf7764361b3fd
Jimmy Vo <jimmy.vo@powerdms222.onmicrosoft.com>
2020-12-07 13:36:51 -0500

    wip

--- SinglePageApp/ClientApp/src/components/getFilteredItems.test.tsx
+++ SinglePageApp/ClientApp/src/components/getFilteredItems.test.tsx
@@ -4,36 +4,36 @@ import { getFilteredItems } from "./MaxPriceItems";
 
 describe(`${getFilteredItems.name} tests`, () => {
 
-  const originalItems:TProductItemDto[] = [
-    {id: 1, name: "test1", price: 1},
-    {id: 2, name: "test2", price: 2},
+  const originalItems: TProductItemDto[] = [
+    { id: 1, name: "test1", price: 1 },
+    { id: 2, name: "test2", price: 2 },
   ]
 
   test.each`
     addedItem                            | testDescription
-    ${{id: 4, name: "test1", price: 0}}} | ${"equal to"}
-    ${{id: 4, name: "test1", price: 1}}} | ${"lower than"}
+    ${{ id: 4, name: "test1", price: 0 }}} | ${"equal to"}
+    ${{ id: 4, name: "test1", price: 1 }}} | ${"lower than"}
   
   `(
     "added item should be part of filtered items when added item's price is $description the highest price",
     ({ addedItem }) => {
-     
+
       var expectItem = getFilteredItems(addedItem, originalItems)
         .find(i => i.id === addedItem.id);
 
       expect(expectItem)
-      .toBe(undefined);
+        .toBe(undefined);
     },
   );
-
+  //Jimmy
   test.each`
     addedItem                            | description
-    ${{id: 4, name: "test1", price: 2}}} | ${"added item's price is highest"}
-    ${{id: 4, name: "test3", price: 2}}} | ${"added item is a new item."}
+    ${{ id: 4, name: "test1", price: 2 }}} | ${"added item's price is highest"}
+    ${{ id: 4, name: "test3", price: 2 }}} | ${"added item is a new item."}
   `(
     "added item should be part of filtered items when $description",
     ({ addedItem }) => {
-     
+
       const expectedItems = originalItems.map(x => x);
       expectedItems.push(addedItem);
 
